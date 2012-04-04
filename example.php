70c71c848400a5ce7d78906f0ce873884a94ff57
AlexeyGreelancer <alexey.freelancer@gmail.com>
2012-04-04 15:24:19 +0300

    added example

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+
+// register autoloader
+// git clone git://github.com/symfony/ClassLoader.git
+require_once 'ClassLoader/UniversalClassLoader.php';
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+$loader = new UniversalClassLoader();
+$loader->register();
+$loader->registerNamespace('BackupTask', '.');
+
+$config = include '/path/to/config.php';
+$backupTask = new BackupTask\BackupTask($config);
+$backupTask->run();
